2229ec645d4bbde68ce7846636eafc37e519fb89
wamahiga <61947672+Wamahiga@users.noreply.github.com>
2022-04-04 01:23:34 +0300

    logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,57 @@
-// Hold Akan names in the global scope
-const akanNames = [
-  {
-      male: "Kwasi",
-      female: "Akosua"
-  },
-  {
-      male: "Kwadwo",
-      female: "Adwoa"
-  },
-  {
-      male: "Kwabena",
-      female: "Abenaa"
-  },
-  {
-      male: "Kwaku",
-      female: "Akua"
-  },
-  {
-      male: "Yaw",
-      female: "Yaa"
-  },
-  {
-      male: "Kofi",
-      female: "Afua"
-  },
-  {
-      male: "Kwame",
-      female: "Ama"
+function akan(){
+  var birthDate=document.getElementById('date').value;
+  var gender=document.getElementById('gender').value;
+  birthDate.toString();
+  if (!birthDate) {
+    alert("You entered an invalid date");
   }
-];
-
+  var centuryYear;
+  var birthYear;
+  var birthMonth;
+  var birthDay;
+  var year;
+  centuryYear=parseInt(birthDate.slice(0, 2));
+  birthYear=parseInt(birthDate.slice(2, 4));
+  birthMonth=parseInt(birthDate.slice(5, 7));
+  birthDay=parseInt(birthDate.slice(8, 10));
+  year=parseInt(birthDate.slice(0, 4));
+  if ((birthDay <= 0 || birthDay> 31) || (birthMonth<= 0 || birthMonth> 12)){
+    alert("You entered an invalid date");
+  }
+  var weekDays;
+  var maleNames;
+  var femaleNames;
+  weekDays=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  maleNames=["Kwasi", "Kwadwo", "Kwabena", "Kwaku","Yaw", "Kofi", "Kwame"];
+  femaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  var dayOfTheWeek;
+  var day;
+  dayOfTheWeek =( ( (centuryYear/4) -2*centuryYear-1) + ((5*birthYear/4) ) + ((26*(birthMonth+1)/10)) + birthDay ) % 7;
+  if (dayOfTheWeek< 0) {
+    day = Math.round(dayOfTheWeek)+7;
+  }else {
+    day = Math.trunc(dayOfTheWeek);
+  }
+  if (year < 1920 || (year > 1920 && year < 1979)){
+day = day -1;
+  }
+  if (year >= 1980 && year < 2000) {
+    day = day -1;
+  }
+  if (year === 1979) {
+    day = day +6;
+  }
+  if (year === 1990) {
+    day = day+ 7;
+  }
+  var akanName;
+  var dayName;
+  if (gender === "male") {
+    akanName = maleNames[day];
+    dayName = weekDays[day];
+  } else {
+    akanName = femaleNames[day];
+    dayName = weekDays[day];
+  }
+  document.getElementById('output').innerHTML = 'Born on ' + dayName +',your Akan name is ' +  akanName;
+}
