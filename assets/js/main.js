c75fd67ce6129380349a66d6069f5f328439220d
wamahiga <61947672+Wamahiga@users.noreply.github.com>
2022-04-03 11:54:38 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+// Hold Akan names in the global scope
+const akanNames = [
+  {
+      male: "Kwasi",
+      female: "Akosua"
+  },
+  {
+      male: "Kwadwo",
+      female: "Adwoa"
+  },
+  {
+      male: "Kwabena",
+      female: "Abenaa"
+  },
+  {
+      male: "Kwaku",
+      female: "Akua"
+  },
+  {
+      male: "Yaw",
+      female: "Yaa"
+  },
+  {
+      male: "Kofi",
+      female: "Afua"
+  },
+  {
+      male: "Kwame",
+      female: "Ama"
+  }
+];
+
